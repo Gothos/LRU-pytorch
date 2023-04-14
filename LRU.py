26b0a88625e65e21e9a21ce81b3aeeb54aa54b28
Vishnu V Jaddipal <95531133+Gothos@users.noreply.github.com>
2023-04-14 16:10:38 +0530

    Add files via upload

---
+++ LRU.py
@@ -0,0 +1,52 @@
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class LRU(nn.Module):
+    def __init__(self,in_features,out_features,state_features, rmin=0, rmax=1,max_phase=6.283):
+        super().__init__()
+        self.D=nn.Parameter(torch.randn([in_features,out_features])/math.sqrt(in_features))
+        u1=torch.rand(state_features)
+        u2=torch.rand(state_features)
+        self.nu_log= nn.Parameter(torch.log(-0.5*torch.log(u1*(rmax+rmin)*(rmax-rmin) + rmin**2)))
+        self.theta_log= nn.Parameter(torch.log(max_phase*u2))
+        Lambda_mod=torch.exp(-torch.exp(self.nu_log))
+        self.gamma_log=nn.Parameter(torch.log(torch.sqrt(torch.ones_like(Lambda_mod)-torch.square(Lambda_mod))))
+        B_re=torch.randn([state_features,in_features])/math.sqrt(2*in_features)
+        B_im=torch.randn([state_features,in_features])/math.sqrt(2*in_features)
+        self.B=nn.Parameter(torch.complex(B_re,B_im))
+        C_re=torch.randn([out_features,state_features])/math.sqrt(state_features)
+        C_im=torch.randn([out_features,state_features])/math.sqrt(state_features)
+        self.C=nn.Parameter(torch.complex(C_re,C_im))
+        self.state=torch.complex(torch.zeros(state_features),torch.zeros(state_features))
+
+    def forward(self, input):
+        self.state=self.state.to(self.B.device)
+        Lambda_mod=torch.exp(-torch.exp(self.nu_log))
+        Lambda_re=Lambda_mod*torch.cos(torch.exp(self.theta_log))
+        Lambda_im=Lambda_mod*torch.sin(torch.exp(self.theta_log))
+        Lambda=torch.complex(Lambda_re,Lambda_im)
+        Lambda=Lambda.to(self.state.device)
+        gammas=torch.exp(self.gamma_log).unsqueeze(-1).to(self.B.device)
+        gammas=gammas.to(self.state.device)
+        #Handle input of (Batches,Seq_length, Input size)
+        if input.dim()==3:
+            output=torch.empty_like(input)
+            for i,batch in enumerate(input):
+                out_seq=torch.empty_like(batch)
+                for j,step in enumerate(batch):
+                    self.state=(Lambda@self.state + gammas* self.B@step.to(dtype= self.B.dtype))
+                    out_step= (self.C@self.state).real + self.D@step
+                    out_seq[j]=out_step
+                self.state=torch.complex(torch.zeros_like(self.state.real),torch.zeros_like(self.state.real))
+                output[i]=out_seq
+        #Handle input of (Seq_length, Input size)
+        if input.dim()==2:
+            output=torch.empty_like(input)
+            for i,step in enumerate(input):
+                self.state=(Lambda@self.state + gammas* self.B@step.to(dtype= self.B.dtype))
+                out_step= (self.C@self.state).real + self.D@step
+                output[i]=out_step
+            self.state=torch.complex(torch.zeros_like(self.state.real),torch.zeros_like(self.state.real))
+        return output
