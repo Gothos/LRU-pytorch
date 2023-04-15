f3086476f5b323e3a1b6f46b7b1f25c51723f0dd
Vishnu V Jaddipal <95531133+Gothos@users.noreply.github.com>
2023-04-15 12:31:58 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+
+from distutils.core import setup
+setup(
+  name = 'LRU-pytorch',         
+  packages = ['LRU-pytorch'],  
+  version = '0.1',     
+  license='MIT',       
+  description = 'TYPE YOUR DESCRIPTION HERE',  
+  author = 'Vishnu Jaddipal',                  
+  author_email = 'your.email@domain.com',     
+  url = 'https://github.com/user/reponame',   
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',  
+  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   
+  install_requires=[            
+          'validators',
+          'beautifulsoup4',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',    
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    
+  ],
+)
